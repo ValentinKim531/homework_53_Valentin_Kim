8983fd85105d466a8452e4a154f52b59138f1d5b
Valentin Kim <valentink@yandex.ru>
2023-02-16 16:26:23 +0600

    реализован функционал редактирования задачи

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views.to_do import add_view, detail_view, delete_task
+from webapp.views.to_do import add_view, detail_view, delete_task, update_view
 from webapp.views.base import index_view
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('to_do/', index_view),
     path('to_do/add', add_view, name='to_do_add'),
     path('to_do/<int:pk>', detail_view, name='to_do_detail'),
-    path('to_do/delete/<int:pk>', delete_task, name='to_do_delete')
+    path('to_do/delete/<int:pk>', delete_task, name='to_do_delete'),
+    path('to_do/update/<int:pk>', update_view, name='to_do_update'),
 ]
--- webapp/views/to_do.py
+++ webapp/views/to_do.py
@@ -26,6 +26,18 @@ def add_view(request: WSGIRequest):
     to_do = To_do.objects.create(**to_do_data)
     return redirect('to_do_detail', pk=to_do.pk)
 
+def update_view(request: WSGIRequest, pk):
+    to_do = get_object_or_404(To_do, pk=pk)
+    if request.method == "GET":
+        return render(request, 'to_do_update.html', context={'to_do': to_do})
+    to_do.title = request.POST.get('title')
+    to_do.description = request.POST.get('description')
+    to_do.text = request.POST.get('text')
+    to_do.status = request.POST.get('status')
+    to_do.execution_date = request.POST.get('execution_date')
+    to_do.save()
+    return redirect('to_do_detail', pk=to_do.pk)
+
 def detail_view(request, pk):
     to_do = get_object_or_404(To_do, pk=pk)
     return render(request, 'to_do.html', context={
@@ -33,6 +45,6 @@ def detail_view(request, pk):
     })
 
 def delete_task(request, pk):
-    to_do = to_do = get_object_or_404(To_do, pk=pk)
+    to_do = get_object_or_404(To_do, pk=pk)
     to_do.delete()
     return redirect('index')
